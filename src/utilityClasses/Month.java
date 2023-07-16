8c32d22b25fe5537e7289f921731dec8174c3b4d
piazimmermann <fhb211637@fh-vie.ac.at>
2023-07-16 20:55:12 +0200

    added lombok functionalities

--- src/utilityClasses/Month.java
+++ src/utilityClasses/Month.java
@@ -1,8 +1,11 @@
 package utilityClasses;
+import lombok.*;
 
 /**
  * Legacy code, not used in program. Creates enums for months
  */
+
+@Getter
 public enum Month {
 
     JANUAR(0), FEBRUAR(1), MAERZ(2), APRIL(3), MAI(4), JUNI(5), JULI(6), AUGUST(7), SEPTEMBER(8), OKTOBER(9), NOVEMBER(10), DEZEMBER(11);
@@ -12,9 +15,4 @@ public enum Month {
     Month(int monthNumber){
         this.monthNumber = monthNumber;
     }
-
-    public int getMonthNumber(){
-        return monthNumber;
-    }
-
 }
